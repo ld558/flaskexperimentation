a1a92b0b7643d278d1da0581b7d74f681cbabf18
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-11 14:23:58 +0100

    implementing html interfaces

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 #Comprises the different URLS that the application implements
 from app import app
-
+from flask import render_template
 @app.route('/')
-@app.route('/index')
-def index():
-    return("Salutations")
+def homepage():
+    season_info={'day':0}
+    team_stats = [
+        {'name':'Clippers','wins': 23},
+        {'name':'Lakers','wins':14}
+    ]
+    return render_template('homepage.html',title='Home', season_info=season_info, team_stats=team_stats)
+
