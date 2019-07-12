c3992bc30b5b6c42a7e822ac3c2ddca96ddf957f
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-12 13:57:20 +0100

    Including greater db features

--- app/models.py
+++ app/models.py
@@ -5,14 +5,24 @@ class Balhead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    predictions = db.relationship('Predictions', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<Balhead: {}>'.format(self.username)
 
 
 class Teams(db.Model):
-    abbreviation = db.Column(db.String(3), primary_key=True)
+    id = db.Column(db.String(3), primary_key=True)
     name = db.Column(db.String(64), unique=True)
-
+    predict_id = db.Column(db.Integer, db.ForeignKey('predictions.id'))
+    predictions = db.relationship('Estimated wins', backref='team', lazy='dynamic')
     def __repr__(self):
         return '<Team: The {}'.format(self.name)
+
+class Predictions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    predicts = db.Column(db.String(60))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return "<{}'s Predictions>".format(self.user_id)
---
+++ app/predictions.py
@@ -0,0 +1,3 @@
+
+class Predictions(dict):
+
