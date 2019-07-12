f9e104993198aa0d41e3717973dfe47f7e9723cc
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-12 11:24:50 +0100

    resolving db errors

--- config.py
+++ config.py
@@ -2,9 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'salutaions'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'salutations'
     # Specifies where to create the database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir,'app.db')
-    # prevents the database from notifying the app at each alteratoin
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
