63ec4032c0c1d7b2afe374e69e4e02f367172d3b
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-30 13:43:44 +0100

    rectifying errors 2

--- app/models.py
+++ app/models.py
@@ -24,7 +24,12 @@ class Predict(db.Model):
     balhead_id = db.Column(db.Integer, db.ForeignKey('balhead.username'))
     team_id = db.Column(db.String(3), db.ForeignKey('team.id'))
     id = db.Column(db.String(70), primary_key=True, index=True)
-    id = balhead_id + " ; " + team_id
+
+    def __init__(self,balhead_id,team_id,prediction):
+        self.balhead_id = balhead_id
+        self.team_id = team_id
+        self.prediction = prediction
+        self.id = balhead_id + " ; " + team_id
 
     def __repr__(self):
         return '<Prediction by {}; Team: {} - Wins: {}>'.format(self.balhead_id,self.team_id,self.prediction)
