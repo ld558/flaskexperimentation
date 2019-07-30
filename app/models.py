a670fb60ba3ee2bd24a764c38845b1698345e772
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-30 12:51:16 +0100

    rectifying errors

--- app/models.py
+++ app/models.py
@@ -14,12 +14,13 @@ class Balhead(db.Model):
 class Team(db.Model):
     id = db.Column(db.String(3), primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    team_predictions = db.Relationship('Predict', backref='team', lazy = 'dynamic')
+    team_predictions = db.relationship('Predict', backref='team', lazy = 'dynamic')
 
     def __repr__(self):
         return '<Team: The {}'.format(self.name)
 
 class Predict(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     prediction = db.Column(db.Integer)
     balhead_id = db.Column(db.Integer, db.ForeignKey('balhead.id'))
     team_id = db.Column(db.String(3), db.ForeignKey('team.id'))
